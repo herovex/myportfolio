46dd2b7bcb4af8acd94be1189ee743e4455d2df2
= <=>
2024-12-17 08:33:04 +0100

    create new html

--- classjs.js
+++ classjs.js
@@ -1,3 +1,20 @@
-for (var i = 1; i <= 12; i++) {
-    console.log("2 X " + i + " = " + 2*i)
+
+//FIZZ BUZZ TO 100 ASSIGHMENT
+  for (let i = 1; i <= 100; i++) {
+    if (i % 3 == 0 && i % 5 == 0) {
+      console.log("FizzBuzz");
+    } else if (i % 3 == 0) {
+      console.log("Fizz");
+    } else if (i % 5 == 0) {
+      console.log("Buzz");
+    } else {
+      console.log(i);
+    }
+  }
+
+//CLASSWORK ON ADDING TWO NUMBERS AND PUT THEM IN A VARIABLE
+function addTwo(a, b) {
+  return a + b;
 }
+var addition = addTwo(3, 5);
+console.log(addition);
